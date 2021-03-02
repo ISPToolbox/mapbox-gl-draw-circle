72406d9526944f8e72f15b717fda5ee68cc5ab60
Alex Chong <achong@fb.com>
2021-03-02 17:16:38 +0000

    export utilities for isptoolbox

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+declare module 'mapbox-gl-draw-circle' {
+    export const CircleMode: any;
+    export const DragCircleMode: any;
+    export const DirectMode: any;
+    export const SimpleSelectMode: any;
+}
